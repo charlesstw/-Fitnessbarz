80b4888a2394aadb1bae5447beaf9cdf68381da1
charlesstw <charlesstw@hotmail.com>
2018-09-20 00:50:07 +0800

    加上icon,初始化tableView

--- FitnessBarz_swift/FitnessBarz/FitnessBarz/Feed/FeedVC.swift
+++ FitnessBarz_swift/FitnessBarz/FitnessBarz/Feed/FeedVC.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class FeedVC: UIViewController {
-
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    var mainTableView:UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         Utils.setUpDefaultVC(viewContrller: self)
         self.navigationController?.setNavigationBarHidden(true, animated: false)
+        setUpTableView()
         
         // Do any additional setup after loading the view.
     }
@@ -22,16 +23,33 @@ class FeedVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    func setUpTableView() {
+        mainTableView = UITableView.init(frame: self.view.frame, style: UITableViewStyle.plain)
+        mainTableView.dataSource = self
+        mainTableView.delegate = self
+        self.view.addSubview(mainTableView)
+        mainTableView.register(UINib(nibName: "PostCell", bundle: nil), forCellReuseIdentifier:"PostCell")
+        
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+//        mainTableView.translatesAutoresizingMaskIntoConstraints = false
+//        let views = ["view": view!, "mainTableView": mainTableView]
+//        let horizontalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "H:[view]-[mainTableView]", options: NSLayoutFormatOptions.alignAllCenterY, metrics: nil, views: views)
+//        let verticalConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:[view]-[mainTableView]", options: NSLayoutFormatOptions.alignAllCenterX, metrics: nil, views: views)
+//        NSLayoutConstraint.activate(horizontalConstraints)
+//        NSLayoutConstraint.activate(verticalConstraints)
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath)
 
+       return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 300
+    }
 }
